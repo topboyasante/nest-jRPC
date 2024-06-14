639f005b3ac24c20795f0a2c20829ca5a1cc314c
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-06-14 17:53:27 +0000

    upd8

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { RpcController } from './rpc/rpc.controller';
-import { JsonRpcRequest } from 'typed-rpc/lib/types';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  const fastifyInstance = app.getHttpAdapter().getInstance();
-
-  fastifyInstance.post('/v1/rpc', async (request, reply) => {
-   const body = request.body as JsonRpcRequest;
-    const { method, params, id } = body;
-
-    const rpcController = app.get(RpcController);
-
-    if (method === 'hello') {
-      const result = await rpcController.hello();
-      reply.send({ jsonrpc: '2.0', result, id });
-    } else {
-      reply.send({ jsonrpc: '2.0', error: 'Method not found', id });
-    }
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
--- src/rpc/rpc.controller.ts
+++ src/rpc/rpc.controller.ts
@@ -1,11 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { FastifyReply } from 'fastify';
+import { JsonRpcRequest } from 'typed-rpc/lib/types';
 import { RpcService } from './rpc.service';
 
 @Controller('/v1/rpc')
 export class RpcController {
   constructor(private readonly rpcService: RpcService) {}
 
-  async hello(){
-    return this.rpcService.hello()
+  @Post()
+  async rpc(@Body() body: JsonRpcRequest, @Res() reply: FastifyReply) {
+    const { method, params, id } = body;
+
+    if (method === 'hello') {
+      const result = this.rpcService.hello();
+      reply.send({ jsonrpc: '2.0', result, id });
+    } else {
+      reply.send({ jsonrpc: '2.0', error: 'Method not found', id });
+    }
   }
 }
